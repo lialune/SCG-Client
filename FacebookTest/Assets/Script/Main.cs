b092aadf2ab4e5cbe81481665856be924275d02d
lialune-PC\lialune <kje48012>
2016-08-29 13:21:09 +0900

    페이스북 로그인 테스트 웹서버 Request및 Response 테스트 Responese 결과 Json으로 받아지는지와 Json파싱법 찾는중

---
+++ FacebookTest/Assets/Script/Main.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Net.j
+using Facebook.Unity;
+
+public class Main : MonoBehaviour
+{
+    public string IP;
+
+    void Start()
+    {
+        //Socket Sock = null;
+        //IPEndPoint EndPoint;
+        //byte[] buf = new byte[1024];
+
+        //Sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //IPAddress addr = IPAddress.Parse("127.0.0.1");
+        //EndPoint = new IPEndPoint(addr, 8080);
+
+        //Sock.Connect(EndPoint);
+
+        //Debug.Log("Connect");
+
+        //Homura.Packet pac = new Homura.Packet();
+        //pac.initialize(Homura.PACKET_HEADER.PH_C_TO_S_CONNECT_CHECK_REQ, 8);
+
+        //Sock.Send(pac.getBuffer());
+
+        //Sock.Close();
+
+        // 웹 통신
+        string uri = "http://" + IP + ":8080/SCG/now.jsp";
+        HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(uri);
+        Request.ContentType = "application/x-www-form-urlencoded; charset=euc-kr;";
+        Request.ContentLength = uri.Length;
+
+        WebResponse Respone = Request.GetResponse();
+        Stream DataStread = Respone.GetResponseStream();
+        StreamReader Reader = new StreamReader(DataStread, System.Text.Encoding.Default);
+        string Data = Reader.ReadToEnd();
+
+        Reader.Close();
+        Respone.Close();
+    }
+
+    void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(InitCallBack, OnHideUnity);
+        }
+        else
+        {
+            FB.ActivateApp();
+        }
+    }
+
+    public void ButtonEventLogin()
+    {
+        List<string> Perms = new List<string>() { "public_profile", "email" };
+
+        FB.LogInWithReadPermissions(Perms, AuthCallback);
+    }
+
+    void InitCallBack()
+    {
+        if (FB.IsInitialized)
+        {
+            FB.ActivateApp();
+        }
+        else
+        {
+            Debug.Log("Failed to Initialize the Facebook SDK");
+        }
+    }
+
+    void OnHideUnity(bool _IsGameShown)
+    {
+        if (!_IsGameShown)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    void AuthCallback(ILoginResult _Result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            Facebook.Unity.AccessToken aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+
+            Debug.Log(aToken.UserId);
+
+            foreach (string perm in aToken.Permissions)
+            {
+                Debug.Log(perm);
+            }
+        }
+        else
+        {
+            Debug.Log("User cancelled login");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ FacebookTest/Assets/Script/Packet.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homura
+{
+    // S : Server
+    // C : Client
+    public enum PACKET_HEADER
+    {
+        PH_C_TO_S_CONNECT_CHECK_REQ,
+        PH_S_TO_C_CONNECT_CHECK_ANS,
+        PH_ERROR_CHECK
+    }
+
+    public enum PACKET_ERROR_CODE
+    {
+        PEC_COMPLITE,
+        PEC_NOT_DEFINE_PACKET_HEADER,
+        PEC_FAIL_CREATE_BUFFER,
+        PEC_NOT_DATA,
+        PEC_ZERO_OR_MINUS_SIZE,
+        PEC_OVER_PULL
+    }
+
+    // パケットの構造はHeader+Size+Dataです。
+    class Packet
+    {
+        public const int PACKET_DEFAULT_SIZE = 1024;
+        public const int PACKET_MIN_SIZE = sizeof(PACKET_HEADER) + sizeof(int);
+
+        byte[] mBuffer;
+        PACKET_HEADER mHeader;
+        int mUsingSize;
+        int mBufferSize;
+        int mReadPos;
+
+        public Packet()
+        {
+        }
+
+        public PACKET_ERROR_CODE initialize(PACKET_HEADER _PacketHeader, int _BufferSize)
+        {
+            int BufferSize = _BufferSize;
+            if (PACKET_MIN_SIZE > _BufferSize)
+            {
+                BufferSize = PACKET_MIN_SIZE;
+            }
+
+            mBuffer = new byte[BufferSize];
+            if (null == mBuffer)
+            {
+                return PACKET_ERROR_CODE.PEC_FAIL_CREATE_BUFFER;
+            }
+            mReadPos = 0;
+            mUsingSize = _BufferSize;
+            mBufferSize = _BufferSize;
+
+            if (0 > mHeader && PACKET_HEADER.PH_ERROR_CHECK <= mHeader)
+            {
+                return PACKET_ERROR_CODE.PEC_NOT_DEFINE_PACKET_HEADER;
+            }
+            mHeader = _PacketHeader;
+            // 임시
+            Buffer.BlockCopy(BitConverter.GetBytes((int)mHeader), 0, mBuffer, 0, 4);
+            Buffer.BlockCopy(BitConverter.GetBytes(mUsingSize), 0, mBuffer, 4, 4);
+
+            return PACKET_ERROR_CODE.PEC_NOT_DEFINE_PACKET_HEADER;
+        }
+
+        public PACKET_ERROR_CODE initialize(byte[] _Buffer, int _BufferSize)
+        {
+            if (null == _Buffer)
+            {
+                return PACKET_ERROR_CODE.PEC_NOT_DATA;
+            }
+
+            if (PACKET_MIN_SIZE > _BufferSize)
+            {
+                return PACKET_ERROR_CODE.PEC_ZERO_OR_MINUS_SIZE;
+            }
+
+            mBuffer = _Buffer;
+            mBufferSize = _BufferSize;
+            mUsingSize = _BufferSize;
+            mReadPos = 8;
+
+            setPacketHeader((PACKET_HEADER)BitConverter.ToInt32(mBuffer, 0));
+
+            return PACKET_ERROR_CODE.PEC_COMPLITE;
+        }
+
+        public PACKET_ERROR_CODE setPacketHeader(PACKET_HEADER _PacketHeader)
+        {
+            if (0 > _PacketHeader && PACKET_HEADER.PH_ERROR_CHECK <= _PacketHeader)
+            {
+                return PACKET_ERROR_CODE.PEC_NOT_DEFINE_PACKET_HEADER;
+            }
+
+            mHeader = _PacketHeader;
+
+            return PACKET_ERROR_CODE.PEC_FAIL_CREATE_BUFFER;
+        }
+
+        public PACKET_HEADER getPacketHeader()
+        {
+            return mHeader;
+        }
+
+        public PACKET_ERROR_CODE append(byte[] _Data, int _Size)
+        {
+            if (null == _Data)
+            {
+                return PACKET_ERROR_CODE.PEC_NOT_DATA;
+            }
+
+            if (0 >= _Size)
+            {
+                return PACKET_ERROR_CODE.PEC_ZERO_OR_MINUS_SIZE;
+            }
+
+            if (mBufferSize - mUsingSize < _Size)
+            {
+                PACKET_ERROR_CODE ErrorCode = BufferSizeUp();
+                if (PACKET_ERROR_CODE.PEC_COMPLITE != ErrorCode)
+                {
+                    return ErrorCode;
+                }
+            }
+
+            Buffer.BlockCopy(_Data, 0, mBuffer, mUsingSize, _Size);
+            mUsingSize += _Size;
+
+            return PACKET_ERROR_CODE.PEC_COMPLITE;
+        }
+
+        public PACKET_ERROR_CODE pull(byte[] _Data, int _Size)
+        {
+            if (mUsingSize - mReadPos < _Size)
+            {
+                return PACKET_ERROR_CODE.PEC_OVER_PULL;
+            }
+
+            Buffer.BlockCopy(mBuffer, mReadPos, _Data, 0, _Size);
+
+            mReadPos += _Size;
+
+            return PACKET_ERROR_CODE.PEC_COMPLITE;
+        }
+
+        public byte[] getBuffer()
+        {
+            return mBuffer;
+        }
+
+        PACKET_ERROR_CODE BufferSizeUp()
+        {
+            byte[] NewBuffer = new byte[mBufferSize * 2];
+
+            if (null == NewBuffer)
+            {
+                return PACKET_ERROR_CODE.PEC_FAIL_CREATE_BUFFER;
+            }
+
+            Buffer.BlockCopy(mBuffer, 0, NewBuffer, 0, NewBuffer.Length);
+            mBuffer = NewBuffer;
+            mBufferSize *= 2;
+
+            return PACKET_ERROR_CODE.PEC_COMPLITE;
+        }
+    }
+}
